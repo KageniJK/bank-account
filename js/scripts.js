a07d8c94ee69cca25138186f3c423679c9626ae5
James Kageni <james.kageni@gmail.com>
2018-07-19 11:55:12 +0300

    Added the deposit function

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,33 @@ $(document).ready(function(){
     event.preventDefault();
 
     var name = $("input#nameInput").val();
-    var startingDeposit = $("input#initialDeposit").val();
+    var startingDeposit = parseInt($("input#initialDeposit").val());
 
     //console.log(name);
-    //console.log(startingDeposit);
+    console.log(typeof startingDeposit);
 
     $("#outputName").text(name);
-    $("#outputBal").text(startingDeposit);
+    $("#outputBal").text(deposit(startingDeposit));
 
   });
 
   $(".transInput form").submit(function(event){
     event.preventDefault();
 
-    var withdraw = $("input#withdraw").val();
-    var deposit = $("input#deposit").val();
+    var withdraw = parseInt($("input#withdraw").val());
+    var depo = parseInt($("input#deposit").val());
 
-    console.log(withdraw);
-    console.log(deposit);
+    console.log(currentBal);
+    $("#outputBal").text(deposit(depo));
   });
 });
+
+
+//back end
+
+ var currentBal = 0;
+
+ function deposit (x){
+   currentBal += x;
+   return currentBal;
+ }
