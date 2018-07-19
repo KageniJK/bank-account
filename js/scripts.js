e1c84c7acec5103906d0a6a03a51be413bc642d8
James Kageni <james.kageni@gmail.com>
2018-07-19 11:22:58 +0300

    Added the display to the account

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  
+  $(".initialInput form").submit(function(event){
+    event.preventDefault();
+
+    var name = $("input#nameInput").val();
+    var startingDeposit = $("input#initialDeposit").val();
+
+    //console.log(name);
+    //console.log(startingDeposit);
+
+    $("#outputName").text(name);
+    $("#outputBal").text(startingDeposit);
+
+  });
 });
